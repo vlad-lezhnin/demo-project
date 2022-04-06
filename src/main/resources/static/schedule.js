7e552e51583fc3d31b419c8e1836fb6a704f77fb
vlad <vlad@spotify.com>
2022-04-06 16:31:10 +0200

    small fix in schedule.js

--- src/main/resources/static/schedule.js
+++ src/main/resources/static/schedule.js
@@ -1,12 +1,14 @@
 window.addEventListener("load", () => {
-  $.get( "/api/schedule", function(lessons) {
-    var listHtml = lessons.map(lesson => `
+  $.get("/api/schedule", function (lessons) {
+    var listHtml = lessons.map(lesson => {
+      var lessonHtml = `
       <div class="lesson-component">
             <h3>${lesson.title}</h3>
             <div>When: <b>${new Date(lesson.dateAndTime).toLocaleString("uk")}</b></div>
             <div>${lesson.description}</div>
-      </div>
-    `).join("");
+      </div>`;
+      return lessonHtml
+    }).join("");
     $(".lessons-list").html(listHtml);
   });
 });
